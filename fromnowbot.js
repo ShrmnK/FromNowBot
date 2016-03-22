f652d7d22ecb687bc5e9ef75c09ec8a6e46132af
Shrmn K <github@shrmn.com>
2016-03-23 00:24:21 +0800

    Check for Telegram API connectivity before continuing

--- fromnowbot.js
+++ fromnowbot.js
@@ -33,81 +33,62 @@ if(Meteor.isServer) {
 		var chrono = Meteor.npmRequire('chrono-node');
 		
 		TelegramBot.token = Meteor.settings.BotAPIKey;
-		TelegramBot.start();
 		
-		TelegramBot.addListener('/start', function(command, username) {
-			return 'Send me a time/date you would like to calculate the difference from now til!';
-		});
-		
-		TelegramBot.addListener('/help', function(command, username) {
-			return 'Send me a time/date you would like to calculate the difference from now til!';
-		});
-		
-		TelegramBot.addListener('/list', function(command, username) {
-			return 'There are no commands! Send me a time/date you would like to calculate the difference from now til!';
-		});
-		
-		/*TelegramBot.addListener('/test', function(command, username) {
-			var commandCat = "";
-			if(command.length >= 2)
-				for(var i = 1; i < command.length; i++)
-					commandCat += " " + command[i];
+		var botUsername = '';
+		const getMe = TelegramBot.method('getMe');
+		if(getMe) {
+			botUsername = getMe.result.username;
+			console.log('Connected to Telegram Bot API successfully with @' + botUsername);
 			
-			return "test: " + commandCat.trim();
-		});*/
-		
-		/*TelegramBot.addListener('/fromnow', function(command, username, message) {
-			if(command.length < 2) return "You have not given any input!";
-			var allText = "";
-			for(var i = 1; i < command.length; i++)
-				allText += " " + command[i];
+			TelegramBot.addListener('/start', function(command, username) {
+				return 'Send me a time/date you would like to calculate the difference from now til!';
+			});
 			
-			const messageDate = new Date(message.date * 1000);
-			var givenDate = chrono.parse(allText, messageDate);
+			TelegramBot.addListener('/help', function(command, username) {
+				return 'Send me a time/date you would like to calculate the difference from now til!';
+			});
 			
-			if(givenDate[0] == null) {
-				Meteor.call("addHistory", username, message.chat.id, allText, messageDate);
-				return "Sorry, I did not understand the date you gave me.";
-			} else {
-				//TelegramBot.send("Raw chrono parse: " + chrono.parseDate(message), chat_id);
-				var m = moment(givenDate[0].start.date());
-				Meteor.call("addHistory", username, message.chat.id, allText, messageDate, givenDate[0].start.date());
-				TelegramBot.send('_' + allText + '_ is *' + m.from(messageDate) + "*\n`" + m.format(Meteor.settings.public.dateDisplayFormat) + '`', message.chat.id, true);
-				//return allText + ' is ' + m.fromNow() + "\n" + m.format(Meteor.settings.public.dateDisplayFormat);
-				return false;
-			}
-		});*/
-		
-		TelegramBot.setCatchAllText(true, function(username, message) {
-			const parseText = message.text;
-			const messageDate = new Date(message.date * 1000);
-			var givenDate = chrono.parse(parseText, messageDate);
+			TelegramBot.addListener('/list', function(command, username) {
+				return 'There are no commands! Send me a time/date you would like to calculate the difference from now til!';
+			});
 			
-			if(givenDate[0] == null) {
-				Meteor.call("addHistory", username, message.chat.id, parseText, messageDate);
-				TelegramBot.send("Sorry, I did not understand the date you gave me.", message.chat.id);
-			} else {
-				var m = moment(givenDate[0].start.date());
-				Meteor.call("addHistory", username, message.chat.id, parseText, messageDate, givenDate[0].start.date());
+			TelegramBot.setCatchAllText(true, function(username, message) {
+				const parseText = message.text.replace('@' + botUsername, '').trim();
+				const messageDate = new Date(message.date * 1000);
+				var givenDate = chrono.parse(parseText, messageDate);
 				
-				var diffSeconds = m.diff(messageDate, 'seconds');
-				if(diffSeconds == 0) return TelegramBot.send("That's right now...", message.chat.id, true);
-				
-				// Parse precise difference
-				var diffPrefix = '';
-				var diffSuffix = '';
-				if(diffSeconds > 0) diffPrefix = ' is in:';
-				else {
-					diffSeconds *= -1;
-					diffSuffix = ' ago';
+				if(givenDate[0] == null) {
+					Meteor.call("addHistory", username, message.chat.id, parseText, messageDate);
+					TelegramBot.send("Sorry, I did not understand the date you gave me.", message.chat.id);
+				} else {
+					var m = moment(givenDate[0].start.date());
+					Meteor.call("addHistory", username, message.chat.id, parseText, messageDate, givenDate[0].start.date());
+					
+					var diffSeconds = m.diff(messageDate, 'seconds');
+					if(diffSeconds == 0) return TelegramBot.send("That's right now...", message.chat.id, true);
+					
+					// Parse precise difference
+					var diffPrefix = '';
+					var diffSuffix = '';
+					if(diffSeconds > 0) diffPrefix = ' is in:';
+					else {
+						diffSeconds *= -1;
+						diffSuffix = ' ago';
+					}
+					
+					TelegramBot.send(parseText + diffPrefix + '\n'
+						+ '*' + moment.duration(diffSeconds, 'seconds').format('y [years], M [months], d [days], h [hours] m [minutes and] s [seconds]') + '*' + diffSuffix
+						+ '\nor more broadly, *' + m.from(messageDate) + "*\n`" + m.format(Meteor.settings.public.dateDisplayFormat) + '`'
+						, message.chat.id, true);
 				}
-				
-				TelegramBot.send(parseText + diffPrefix + '\n'
-					+ '*' + moment.duration(diffSeconds, 'seconds').format('y [years], M [months], d [days], h [hours] m [minutes and] s [seconds]') + '*' + diffSuffix
-					+ '\nor more broadly, *' + m.from(messageDate) + "*\n`" + m.format(Meteor.settings.public.dateDisplayFormat) + '`'
-					, message.chat.id, true);
-			}
-		});
+			});
+			
+			TelegramBot.start();
+			console.log('Polling started');
+			
+		} else {
+			console.log('Sorry, it seems that there are errors connecting to the Telegram API. Are you sure you have supplied the API key?');
+		}
 	});
 	
 	Meteor.publish("history", function () {
